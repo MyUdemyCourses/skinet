6b94b7b85f8e2982a62ce2b625602edfd6df2d06
cheahchoon <cheahchoon@mail.com>
2022-01-20 14:05:51 +0800

    end of session 6

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using System.Reflection;
+using System.Reflection.Metadata;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,8 +23,23 @@ namespace Infrastructure.Data
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
+            if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+            {
+                foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+                {
+                    var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType == typeof(decimal));
+
+                    foreach (var property in properties)
+                    {
+                        modelBuilder.Entity(entityType.Name).Property(property.Name)
+                        .HasConversion<double>();
+                    }
+                }
+
+            }
+
         }
 
-        
+
     }
 }
