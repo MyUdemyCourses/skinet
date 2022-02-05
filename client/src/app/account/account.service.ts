af60fa8530ea6a1ac00f5998f55fd4ce024adce8
cheahchoon <cheahchoon@mail.com>
2022-02-05 12:18:31 +0800

    End of section 19

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -1,3 +1,4 @@
+import { IAddress } from './../shared/models/address';
 import { IUser } from './../shared/models/user';
 import { Router } from '@angular/router';
 import { Observable, BehaviorSubject, ReplaySubject, of } from 'rxjs';
@@ -67,5 +68,13 @@ export class AccountService {
 
   checkEmailExists(email: string) {
     return this.http.get(this.baseUrl + "account/emailexists?email=" + email);
+  }
+
+  getUserAddress() {
+    return this.http.get<IAddress>(this.baseUrl + 'account/address');
+  }
+
+  updateUserAddress(address: IAddress) {
+    return this.http.put<IAddress>(this.baseUrl + 'account/address', address);
   }
 }
