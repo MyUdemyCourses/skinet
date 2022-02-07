831dc4c6535d18dd1aa7cb2dbc91a032af7ad669
cheahchoon <cheahchoon@mail.com>
2022-02-07 17:45:05 +0800

    End of section 22

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -38,6 +38,7 @@ namespace API.Controllers
 
         }
 
+        [Cached(600)]
         [HttpGet]
         public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts(
             [FromQuery] ProductSpecParams productParams)
@@ -56,6 +57,7 @@ namespace API.Controllers
             productParams.PageSize, totalItems, data));
         }
 
+        [Cached(600)]
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
@@ -70,6 +72,7 @@ namespace API.Controllers
 
         }
 
+        [Cached(600)]
         [HttpGet("brands")]
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
         {
@@ -77,6 +80,7 @@ namespace API.Controllers
 
         }
 
+        [Cached(600)]
         [HttpGet("types")]
         public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
         {
